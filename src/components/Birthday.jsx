dd4006ec7861ff451040de1d0cb375efc5572641
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-08-17 20:20:38 -0300

    fixed language spelling in Birthday component

--- src/components/Birthday.jsx
+++ src/components/Birthday.jsx
@@ -9,14 +9,14 @@ const Birthday = ( {nombre,fecha,setBirthdayList} ) => {
   const nextBirthday = isPast(birthdayThisYear) ? setYear(birthdayThisYear,actualYear + 1) : birthdayThisYear; // sets the next birthday, wether's in this or the next year
   function showRemainingDays (nextBirthday) { 
     const daysToNextBirthday = parseInt(formatDistanceStrict(today,nextBirthday,{unit:"day"}).split(" ")[0]); // retorna cantidad de dias "in x days", se extrae x
-    return isTomorrow(nextBirthday) ? `tomorrow!` : `en ${daysToNextBirthday} día${ daysToNextBirthday > 1 ? "s" : ""}`; // devuelve string segun corresponda
+    return isTomorrow(nextBirthday) ? `mañana!` : `en ${daysToNextBirthday} día${ daysToNextBirthday > 1 ? "s" : ""}`; // devuelve string segun corresponda
   }
   const capitalizeName = name => `${name[0].toUpperCase()}${name.slice(1)}`
   function deleteBirthday(event) {
     const clickedFriendName = event.target.previousSibling.wholeText.split(" ")[2];
     setBirthdayList( prevList => prevList.filter( friend => capitalizeName(friend.nombre) !== capitalizeName(clickedFriendName)))
   }
-  const daysToDisplay = isToday(birthdayThisYear) ? "today!" : showRemainingDays(nextBirthday);
+  const daysToDisplay = isToday(birthdayThisYear) ? "hoy!" : showRemainingDays(nextBirthday);
   return (
     <li className="birthday-item">
         Tu amigx {capitalizeName(nombre)} cumple {daysToDisplay} 
